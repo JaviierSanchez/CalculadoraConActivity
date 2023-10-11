5603fea3602d09d559e6d435a7c5bcad39f7143f
Javiersc647 <javiersc16@educarex.es>
2023-10-11 13:44:27 +0200

    a

--- app/src/main/java/com/example/sumador/MainActivity.java
+++ app/src/main/java/com/example/sumador/MainActivity.java
@@ -1,38 +1,94 @@
 package com.example.sumador;
 
+import static android.app.PendingIntent.getActivity;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public int Resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-public void sumar(View vista){
+    public void operar(View vista) {
+        int numero1 = 0;
+        int numero2 = 0;
+        int res = 0;
+        RadioButton sum = (RadioButton) findViewById(R.id.radioButtonSumar);
+        RadioButton restar = (RadioButton) findViewById(R.id.radioButtonRestar);
 
-        EditText valor = (EditText)findViewById(R.id.editTextNumberValor1);
-        int numero1 = Integer.parseInt(valor.getText().toString());
+        EditText valor1 = (EditText) findViewById(R.id.editTextValor1);
+        try {
+            numero1 = Integer.parseInt(valor1.getText().toString());
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "INTRODUCE NUMERO!!!", Toast.LENGTH_SHORT).show();
+        }
+        EditText valor2 = (EditText) findViewById(R.id.editTextValor2);
+        try {
+            numero2 = Integer.parseInt(valor2.getText().toString());
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "INTRODUCE NUMERO!!!", Toast.LENGTH_SHORT).show();
+        }
 
-        EditText valor2 = (EditText) findViewById(R.id.editTextNumberSegundoValor);
-        int numero2 = Integer.parseInt(valor2.getText().toString());
+        if (sum.isChecked()) {
 
-
-
-        Resultado = numero1 + numero2;
-
-        TextView myTextView = (TextView) findViewById(R.id.textView8);
-        String cadena = String.valueOf(Resultado);
+            res = numero1 + numero2;
+        }
+        if (restar.isChecked()) {
+            res = numero1 - numero2;
+        }
+        TextView myTextView = (TextView) findViewById(R.id.editTextResultado);
+        String cadena = String.valueOf(res);
         myTextView.setText(cadena);
-}
+
+    }
+
+    public void acercaDe(View vista) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Programador")
+                .setMessage("Javier Sánchez Cerrato")
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
 
+                    }
+                });
 
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void salirApp(View vista) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Salir")
+                .setMessage("¿Estás seguro de que quieres salir?")
+                .setNegativeButton("Salir", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+
+                    }
+
+
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
 }
